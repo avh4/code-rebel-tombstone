83a8db660361117361558bd6b7eb6a94dc5b3c83
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-07-13 23:40:52 -0700

    Remove unused code and rename actions -> tasks

--- src/main/js/MyProject.js
+++ src/main/js/MyProject.js
@@ -3,20 +3,19 @@
 "use strict";
 
 var React = require('react');
-var Label = require('./Label');
 var ReactFireMixin = require('reactfire');
 
 module.exports = React.createClass({
   mixins: [ReactFireMixin],
   getInitialState: function() {
-    return { actions: [] };
+    return { tasks: [] };
   },
   componentWillMount: function() {
-    this.bindAsArray(new Firebase('https://rebel-tombstone-dev.firebaseio.com/actions'), 'actions');
+    this.bindAsArray(new Firebase('https://rebel-tombstone-dev.firebaseio.com/tasks'), 'tasks');
   },
   render: function() {
-    var list = this.state.actions.map(function(action) {
-      return <li className="list-group-item">{action.description}</li>;
+    var list = this.state.tasks.map(function(task) {
+      return <li className="list-group-item">{task.description}</li>;
     })
     return <div>
       <h2>Tasks</h2>
