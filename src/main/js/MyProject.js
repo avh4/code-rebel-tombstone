7d35078731e884dbadd88b52028e9707a8ddb854
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-07-13 23:33:20 -0700

    Show list of actions

--- src/main/js/MyProject.js
+++ src/main/js/MyProject.js
@@ -4,9 +4,23 @@
 
 var React = require('react');
 var Label = require('./Label');
+var ReactFireMixin = require('reactfire');
 
 module.exports = React.createClass({
+  mixins: [ReactFireMixin],
+  getInitialState: function() {
+    return { actions: [] };
+  },
+  componentWillMount: function() {
+    this.bindAsArray(new Firebase('https://rebel-tombstone-dev.firebaseio.com/actions'), 'actions');
+  },
   render: function() {
-    return <div><Label>MyProject</Label></div>;
+    var list = this.state.actions.map(function(action) {
+      return <li>{action.description}</li>;
+    })
+    return <div>
+      <Label>MyProject</Label>
+      <ul>{list}</ul>
+    </div>;
   }
 });
