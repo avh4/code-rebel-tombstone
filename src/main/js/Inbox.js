18c521dd17f442e74df9b621fe315448d6b51e47
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-07-15 11:48:12 -0700

    Inbox items can be moved to projects

--- src/main/js/Inbox.js
+++ src/main/js/Inbox.js
@@ -5,6 +5,8 @@
 var React = require('react');
 var ReactFireMixin = require('reactfire');
 
+var Typeahead = require('./Typeahead');
+
 module.exports = React.createClass({
   mixins: [ReactFireMixin],
   getInitialState: function() {
@@ -13,17 +15,47 @@ module.exports = React.createClass({
   componentWillMount: function() {
     this.bindAsArray(new Firebase('https://rebel-tombstone-dev.firebaseio.com/projects'), 'projects');
     this.bindAsArray(new Firebase('https://rebel-tombstone-dev.firebaseio.com/inbox'), 'inbox');
+    
+    new Firebase('https://rebel-tombstone-dev.firebaseio.com/inbox').startAt().limit(1).on('value', function(v) {
+      v.forEach(function(c) {
+        this.setState({ next: c });
+      }.bind(this));
+    }.bind(this));
+  },
+  doSubmit: function() {
+    var item = this.state.inbox[0];
+    var p = this.refs.project.value();
+    var newTask = this.firebaseRefs.projects.child(p.id).child('tasks').push();
+    newTask.set(item, function(error) {
+      if (error) alert(error);
+      else {
+        this.firebaseRefs.inbox.child(this.state.next.name()).remove();
+      }
+    }.bind(this));
   },
   render: function() {
+    var projects = [];
+    if (this.state.projectsSnap) {
+      this.state.projectsSnap.forEach(function(p) {
+        projects.push({ title: p.val().title, id: p.name() });
+      });
+    }
+
     var list = [];
-    if (this.state.inbox.length <= 0) return <div/>;
-    return <div className="panel panel-default">
-      <div className="panel-heading">
-        <h3 className="panel-title">{this.state.inbox[0].description}</h3>
+    if (!this.state.next) return <div/>;
+    return <div>
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h3 className="panel-title">{this.state.next.val().description}</h3>
+        </div>
+        <div className="panel-body">
+          <form onSubmit={this.doSubmit}>
+            <Typeahead ref="project" placeholder="Project" options={projects} displayKey="title"/>
+            <button className="btn btn-primary">Make Action</button>
+          </form>
+        </div>
       </div>
-      <div className="panel-body">
-        Panel content
-      </div>
+    <p>{this.state.inbox.length - 1} more</p>
     </div>;
   }
 });
---
+++ src/main/js/Typeahead.js
@@ -0,0 +1,36 @@
+/** @jsx React.DOM */
+
+"use strict";
+
+var React = require('react');
+
+module.exports = React.createClass({
+  componentDidMount: function() {
+    var source = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace(this.props.displayKey),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      local: this.props.options
+    });
+    source.initialize();
+    
+    $(this.getDOMNode()).typeahead({
+      hint: true,
+      highlight: true,
+      minLength: 1
+    },
+    {
+      displayKey: this.props.displayKey,
+      source: source.ttAdapter()
+    }).on('typeahead:selected', function (obj, datum) {
+      this._v = datum;
+    }.bind(this)).on('typeahead:autocompleted', function (obj, datum) {
+      this._v = datum;
+    }.bind(this));
+  },
+  value: function() {
+    return this._v;
+  },
+  render: function() {
+    return <input ref="input" className="form-control" type="text" placeholder={this.props.placeholder}/>;
+  }
+});
