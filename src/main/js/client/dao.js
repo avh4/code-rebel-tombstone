acc4b9f281ddd3c28e15dd59e88101944a468d79
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-07-24 20:11:55 -0700

    Refactor client dao

--- src/main/js/client/dao.js
+++ src/main/js/client/dao.js
@@ -1,60 +1,63 @@
-var dao = {
-  bindProjects: function(fn) {
-    var projectsRef = new Firebase('https://rebel-tombstone-dev.firebaseio.com/projects');
-    projectsRef.on('value', function(projectsSnap) {
-      fn(projectsSnap);
-    });
-  },
-  bindInboxCount: function(fn) {
-    var inboxRef = new Firebase('https://rebel-tombstone-dev.firebaseio.com/inbox');
-    inboxRef.on('value', function(inboxSnap) {
-      fn(inboxSnap.numChildren());
-    });
-  },
-  bindNextInboxItem: function(fn) {
-    var inboxRef = new Firebase('https://rebel-tombstone-dev.firebaseio.com/inbox');
-    inboxRef.startAt().limit(1).on('value', function(inboxSnap) {
-      inboxSnap.forEach(function(inboxItemSnap) {
-        fn(inboxItemSnap);
-      });
-    });
-  },
-  doMakeTaskFromInboxItem: function(inboxItemSnap, projectOrName) {
-    var projectsRef = new Firebase('https://rebel-tombstone-dev.firebaseio.com/projects');
-    var inboxRef = new Firebase('https://rebel-tombstone-dev.firebaseio.com/inbox');
-    var item = inboxItemSnap.val();
-    var projectRef;
-    if (typeof projectOrName === 'string') {
-      projectRef = projectsRef.push();
-      projectRef.set({title: projectOrName});
-    } else {
-      projectRef = projectsRef.child(projectOrName.id);
-    }
-    var newTask = projectRef.child('tasks').push();
-    newTask.set(item, function(error) {
-      if (error) alert(error);
-      else {
-        inboxRef.child(inboxItemSnap.name()).remove();
-      }
-    });
-  },
-  getTaskRef: function(projectId, taskId) {
-    var completionsRef = new Firebase('https://rebel-tombstone-dev.firebaseio.com/jobs/completions');
-    var projectsRef = new Firebase('https://rebel-tombstone-dev.firebaseio.com/projects');
-    return {
-      doComplete: function(b) {
-        var taskRef = projectsRef.child(projectId).child('tasks').child(taskId);
-        taskRef.child('completed').set(b, function() {
-          taskRef.once('value', function(taskSnap) {
-            var pocketId = taskSnap.child('pocket__item_id').val();
-            if (pocketId) {
-              completionsRef.child('pocket').child('pocket:' + pocketId).set(taskSnap.val());
-            }
-          });
-        });
-      }
-    };
-  }
+
+function dao(ref) {
+  this.ref = ref;
+  this.projectsRef = ref.child('projects');
+  this.inboxRef = ref.child('inbox');
+}
+
+dao.prototype.bindProjects = function(fn) {
+  this.projectsRef.on('value', function(projectsSnap) {
+    fn(projectsSnap);
+  });
 };
 
-module.exports = dao;
+dao.prototype.bindInboxCount = function(fn) {
+  this.inboxRef.on('value', function(inboxSnap) {
+    fn(inboxSnap.numChildren());
+  });
+};
+
+dao.prototype.bindNextInboxItem = function(fn) {
+  this.inboxRef.startAt().limit(1).on('value', function(inboxSnap) {
+    inboxSnap.forEach(function(inboxItemSnap) {
+      fn(inboxItemSnap);
+    });
+  });
+};
+
+dao.prototype.doMakeTaskFromInboxItem = function(inboxItemSnap, projectOrName) {
+  var item = inboxItemSnap.val();
+  var projectRef;
+  if (typeof projectOrName === 'string') {
+    projectRef = this.projectsRef.push();
+    projectRef.set({title: projectOrName});
+  } else {
+    projectRef = this.projectsRef.child(projectOrName.id);
+  }
+  var newTask = projectRef.child('tasks').push();
+  newTask.set(item, function(error) {
+    if (error) alert(error);
+    else {
+      this.inboxRef.child(inboxItemSnap.name()).remove();
+    }
+  }.bind(this));
+};
+
+dao.prototype.getTaskRef = function(projectId, taskId) {
+  var completionsRef = this.ref.child('jobs').child('completions');
+  return {
+    doComplete: function(b) {
+      var taskRef = this.projectsRef.child(projectId).child('tasks').child(taskId);
+      taskRef.child('completed').set(b, function() {
+        taskRef.once('value', function(taskSnap) {
+          var pocketId = taskSnap.child('pocket__item_id').val();
+          if (pocketId) {
+            completionsRef.child('pocket').child('pocket:' + pocketId).set(taskSnap.val());
+          }
+        });
+      });
+    }.bind(this)
+  };
+};
+
+module.exports = new dao(new Firebase('https://rebel-tombstone-dev.firebaseio.com'));
