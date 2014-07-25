d75709b2c5af5eb5dc401dd216083d5946da307d
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-07-24 20:03:27 -0700

    Authenticate the server and clients with firebase

--- src/main/js/client/main.js
+++ src/main/js/client/main.js
@@ -5,4 +5,17 @@
 var React = require('react');
 var MyProject = require('./MyProject');
 
-React.renderComponent(<MyProject/>, document.getElementById('root'));
+var chatRef = new Firebase('https://rebel-tombstone-dev.firebaseio.com');
+var auth = new FirebaseSimpleLogin(chatRef, function(error, user) {
+  if (!user) {
+    console.log(error, user);
+    var email = window.prompt("Email/username");
+    var password = window.prompt("Password");
+    auth.login('password', {
+      email: email,
+      password: password
+    });
+  } else {
+    React.renderComponent(<MyProject/>, document.getElementById('root'));
+  }
+});
--- src/main/js/server/main.js
+++ src/main/js/server/main.js
@@ -4,6 +4,27 @@ var Firebase = require('firebase');
 
 var pocket = require('./jobs/pocket');
 
-processJobs(new Firebase('https://rebel-tombstone-dev.firebaseio.com/integrations/pocket'), pocket);
 
-consumeJobs(new Firebase('https://rebel-tombstone-dev.firebaseio.com/jobs/completions/pocket'), require('./jobs/pocket/completions'));
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Firebase secret: ", function(secret) {
+  var FirebaseTokenGenerator = require("firebase-token-generator");
+  var tokenGenerator = new FirebaseTokenGenerator(secret);
+  var token = tokenGenerator.createToken({ host: require('os').hostname() });
+  new Firebase('https://rebel-tombstone-dev.firebaseio.com').auth(token, function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      processJobs(new Firebase('https://rebel-tombstone-dev.firebaseio.com/integrations/pocket'), pocket);
+
+      consumeJobs(new Firebase('https://rebel-tombstone-dev.firebaseio.com/jobs/completions/pocket'), require('./jobs/pocket/completions'));
+    }
+  });
+
+  rl.close();
+});
